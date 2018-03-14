7963d30d33a90bc6ebce63f39eeaace7e8b5cb3b
Ben Hope <bnjhope@gmail.com>
2018-03-14 14:40:24 +0000

    fix benching message for dc pipeline

--- src/Bench/FFTControlBench.hs
+++ src/Bench/FFTControlBench.hs
@@ -15,19 +15,19 @@ main = defaultMain [
                     bench "orig" $ nf fft samples32768,
                     bench "bflySPipeline" $ nf fftbflySPipeline samples32768,
 		    bench "dc" $ nf fftdc samples32768,
-		    bench "dc" $ nf fftdcPipeline samples32768
+		    bench "dcPipeline" $ nf fftdcPipeline samples32768
                 ],
                 bgroup "FFTControl65536" [
                     bench "orig" $ nf fft samples65536,
                     bench "bflySPipeline" $ nf fftbflySPipeline samples65536,
 		    bench "dc" $ nf fftdc samples65536,
-		    bench "dc" $ nf fftdcPipeline samples65536
+		    bench "dcPipeline" $ nf fftdcPipeline samples65536
                 ],
                 bgroup "FFTControl131072" [
                     bench "orig" $ nf fft samples131072,
                     bench "bflySPipeline" $ nf fftbflySPipeline samples131072,
 		    bench "dc" $ nf fftdc samples131072,
-		    bench "dc" $ nf fftdcPipeline samples131072
+		    bench "dcPipeline" $ nf fftdcPipeline samples131072
                 ]
             ]
     ]
