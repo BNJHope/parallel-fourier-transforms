d5f82b5de2ab480977bd416252046511554e0c48
Ben Hope <bnjhope@gmail.com>
2018-03-14 15:39:00 +0000

    parmap in mapping for map reduce

--- src/FFT/DFTData.hs
+++ src/FFT/DFTData.hs
@@ -13,8 +13,9 @@ import Strategies
 tw :: Int -> Int -> Complex Float
 tw n k = cis (-2 * pi * fromIntegral k / fromIntegral n)
 
-dftInnerMap xs = [ sum (parmap (\j -> xs!!j * tw n (j*k)) [0..n']) | k <- [0..n']]
+dftInnerMap xs = [ sum (innerMap k) | k <- [0..n']]
   where
+    innerMap k = parmap (\j -> xs!!j * tw n (j*k)) [0..n']
     n = length xs
     n' = n-1
 
@@ -30,7 +31,7 @@ dftOuterMap xs = parmap innerFunc [0..n']
         n = length xs
         n' = n-1
 
-dftInnerMapReduce xs = map (\k -> parMapReduceSimple rdeepseq (mapper k) rdeepseq reducer [0..n']) [0..n']
+dftInnerMapReduce xs = parmap (\k -> parMapReduceSimple rdeepseq (mapper k) rdeepseq reducer [0..n']) [0..n']
     where
         mapper k j = xs!!j * tw n (j*k)
         reducer = sum
